ed2d7c936452bbd7bb45f68179d970e4fe002277
Luna78453 <34612255+HLuna78453@users.noreply.github.com>
2024-11-21 18:13:29 -0600

    Implementacion Firebase (1)

--- RateYourMusicApp/src/main.ts
+++ RateYourMusicApp/src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"rymapp-unofficial-1948924","appId":"1:584881246596:web:1205d5ffaec9b3b5c0cdd1","storageBucket":"rymapp-unofficial-1948924.firebasestorage.app","apiKey":"AIzaSyDUpSHZSeDA_x_ZmIJ2_w2fNPBzuUsFxIw","authDomain":"rymapp-unofficial-1948924.firebaseapp.com","messagingSenderId":"584881246596"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
