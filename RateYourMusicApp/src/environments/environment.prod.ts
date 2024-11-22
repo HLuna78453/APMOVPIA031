6a1e6728309a2d298d32ccfcf564e46ea4a35e7f
Luna78453 <34612255+HLuna78453@users.noreply.github.com>
2024-11-21 18:12:25 -0600

    Implementacion Firebase

--- RateYourMusicApp/src/environments/environment.prod.ts
+++ RateYourMusicApp/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyDUpSHZSeDA_x_ZmIJ2_w2fNPBzuUsFxIw",
+  
+    authDomain: "rymapp-unofficial-1948924.firebaseapp.com",
+  
+    projectId: "rymapp-unofficial-1948924",
+  
+    storageBucket: "rymapp-unofficial-1948924.firebasestorage.app",
+  
+    messagingSenderId: "584881246596",
+  
+    appId: "1:584881246596:web:1205d5ffaec9b3b5c0cdd1"
+  
+  } 
 };
--- RateYourMusicApp/src/environments/environment.ts
+++ RateYourMusicApp/src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyDUpSHZSeDA_x_ZmIJ2_w2fNPBzuUsFxIw",
+  
+    authDomain: "rymapp-unofficial-1948924.firebaseapp.com",
+  
+    projectId: "rymapp-unofficial-1948924",
+  
+    storageBucket: "rymapp-unofficial-1948924.firebasestorage.app",
+  
+    messagingSenderId: "584881246596",
+  
+    appId: "1:584881246596:web:1205d5ffaec9b3b5c0cdd1"
+  
+  }
 };
 
 /*
